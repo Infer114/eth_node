a605623d19e1060afd1c516937c2072041f31efe
Infer114 <122565624+Infer114@users.noreply.github.com>
2023-02-04 19:04:28 +0100

    Update lighthouse_update.sh

--- lighthouse_update.sh
+++ lighthouse_update.sh
@@ -9,31 +9,28 @@
 #
 #By Infer114 02-04-2023
 
-#downloading the last version of lighthouse
+echo "downloading the last version of lighthouse"
 curl -LO https://github.com/sigp/lighthouse/releases/download/v3.4.0/lighthouse-v3.4.0-x86_64-unknown-linux-gnu.tar.gz
 
-#stopping node services
+echo "stopping node services"
 sudo systemctl stop lighthousevalidator
 sudo systemctl stop lighthousebeacon
 sudo systemctl stop geth
 sudo systemctl stop mevboost
-read a
 
-#unzipping and replacing the version
+echo "unzipping and replacing the version"
 tar xvf lighthouse-v3.4.0-x86_64-unknown-linux-gnu.tar.gz
 sudo cp lighthouse /usr/local/bin
 
-#starting all node services
+echo "starting all node services"
 sudo systemctl start geth
 sudo systemctl start lighthousebeacon
 sudo systemctl start lighthousevalidator
 sudo systemctl start mevboost
-read a
 
-#cleaning the download files
+echo "cleaning the download files"
 sudo rm lighthouse
 sudo rm lighthouse-v3.4.0-x86_64-unknown-linux-gnu.tar.gz
 
-#checking if the version is correctly installed
+echo "checking if the version is correctly installed"
 /usr/local/bin/lighthouse --version
-read a
