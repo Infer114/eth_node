aaf05b96c422eb70000f687026a27f9819e580be
Infer114 <122565624+Infer114@users.noreply.github.com>
2023-02-08 14:51:26 +0100

    Create mev_update.sh

---
+++ mev_update.sh
@@ -0,0 +1,11 @@
+#working on it, not tested
+
+cd ~
+wget https://github.com/flashbots/mev-boost/releases/download/v1.4.0/mev-boost_1.4.0_linux_amd64.tar.gz
+sha256sum mev-boost_1.4.0_linux_amd64.tar.gz
+tar xvf mev-boost_1.4.0_linux_amd64.tar.gz
+sudo systemctl stop mevboost
+sudo cp mev-boost /usr/local/bin
+rm mev-boost LICENSE README.md mev-boost_1.4.0_linux_amd64.tar.gz
+sudo chown mevboost:mevboost /usr/local/bin/mev-boost
+sudo systemctl start mevboost
