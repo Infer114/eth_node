3e0625f6daa0f5aa70f16296bce01be553ab173c
Infer114 <122565624+Infer114@users.noreply.github.com>
2023-02-25 15:03:54 +0100

    Update mev_update.sh

--- mev_update.sh
+++ mev_update.sh
@@ -1,7 +1,8 @@
-#working on it, not tested !
+#just a script for mevboost installation
+#you have to edit your execution/consensus client to make use of this
 
 wget https://github.com/flashbots/mev-boost/releases/download/v1.4.0/mev-boost_1.4.0_linux_amd64.tar.gz
-sha256sum mev-boost_1.4.0_linux_amd64.tar.gz
+#sha256sum mev-boost_1.4.0_linux_amd64.tar.gz
 tar xvf mev-boost_1.4.0_linux_amd64.tar.gz
 sudo systemctl stop mevboost
 sudo cp mev-boost /usr/local/bin
