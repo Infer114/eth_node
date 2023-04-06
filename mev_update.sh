cfbff2f2d823364c18192015972cd8a3816d75b8
Infer114 <122565624+Infer114@users.noreply.github.com>
2023-04-06 15:29:38 +0200

    Update mev_update.sh

--- mev_update.sh
+++ mev_update.sh
@@ -1,4 +1,4 @@
-#just a script for mevboost update
+#just some commands for mevboost update
 
 wget https://github.com/flashbots/mev-boost/releases/download/v1.4.0/mev-boost_1.4.0_linux_amd64.tar.gz
 #sha256sum mev-boost_1.4.0_linux_amd64.tar.gz
